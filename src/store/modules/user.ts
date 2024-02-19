066e65b9f64a8272fd6b69549d7cbb61176726d7
王子一 <wangzy1@nancal.com>
2024-02-19 17:15:42 +0800

    add: dist

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -55,6 +55,7 @@ export const useUserStore = defineStore({
       return new Promise<UserResult>((resolve, reject) => {
         getLogin(data)
           .then(data => {
+            debugger;
             if (data) {
               setToken(data.data);
               resolve(data);
